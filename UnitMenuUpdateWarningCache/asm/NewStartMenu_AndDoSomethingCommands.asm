f092a10bda22d34ce7413e6fcdd65f02393ef8a6
AuraWolfOrSomething <66816675+AuraWolfOrSomething@users.noreply.github.com>
2025-05-18 22:30:20 -0500

    Unit Menu bug fix
    Unit Menu Update Warning Cache: Fixed an issue when drawing the unit menu on the right side of the screen

--- UnitMenuUpdateWarningCache/asm/NewStartMenu_AndDoSomethingCommands.asm
+++ UnitMenuUpdateWarningCache/asm/NewStartMenu_AndDoSomethingCommands.asm
@@ -1,14 +1,6 @@
 .thumb
 
-.macro blh to, reg=r3
-  ldr \reg, =\to
-  mov lr, \reg
-  .short 0xf800
-.endm
-
-.equ StartMenuAdjusted, 0x0804EB98
-.equ gMenuYCoordRepositioning, 0x085B6550
-.equ DisplayUnitAdditionalBlinkingIcons, 0x080275E8
+.include "../UnitMenuUpdateWarningCacheDefs.s"
 
 .global NewStartMenu_AndDoSomethingCommands
 .type NewStartMenu_AndDoSomethingCommands, %function
